8bf59a1370548e67e9acf4fa132cbc36c038bf42
Michael Pellegrini <michael@cappers.ai>
2025-01-29 11:48:50 -0800

    Stream events on home page and link to event details page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,8 +2,9 @@ import { fetchAllEvents } from '$lib/server/remote-events';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-    const loadedEvents = await fetchAllEvents();
     return {
-        events: loadedEvents
+        streamed: {
+            events: fetchAllEvents()
+        }
     }
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { PageData } from "./$types";
 
-    let {data}: {data: PageData} = $props();
+  let {data}: {data: PageData} = $props();
 </script>
 
 <div class="container mx-auto max-w-3xl p-6">
@@ -11,17 +11,23 @@
     </p>
 
     <div class="flex justify-between items-center mt-10">
-        <h1 class="text-2xl font-semibold text-gray-200">Events</h1>
+        <h2 class="text-2xl font-semibold text-gray-200">Events</h2>
         <a class="btn btn-primary text-white" href="/newevent" role="button">➕ Add Event</a>
     </div>
 
     <div class="mt-6 space-y-4">
-        {#each data.events as event}
-            <div class="card bg-base-100 shadow-md p-4">
-                <h2 class="text-lg font-bold text-white">{event.id}: {event.title}</h2>
-                <p class="text-gray-300">{event.description || "No description provided."}</p>
-                <p class="text-sm text-gray-400">{event.date}</p>
-            </div>
-        {/each}
+        {#await data.streamed.events}
+            <p class="text-gray-400">Loading events...</p>
+        {:then events}
+            {#each events as event}
+                <div class="card bg-base-100 shadow-md p-4">
+                    <h2 class="text-lg font-bold">
+                        <a href="/{event.id}" class="text-primary hover:underline">{event.id}: {event.title}</a>
+                    </h2>
+                    <p class="text-gray-300">{event.description || "No description provided"}</p>
+                    <p class="text-sm text-gray-400">{event.date}</p>
+                </div>
+            {/each}
+        {/await}
     </div>
 </div>
